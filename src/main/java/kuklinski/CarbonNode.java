8c21479391dd2216f912fffe95d46d773e6c0175
Riczard <riczardkiller@gmail.com>
2019-11-28 15:39:08 +0100

    add bonds field

--- src/main/java/kuklinski/CarbonNode.java
+++ src/main/java/kuklinski/CarbonNode.java
@@ -8,14 +8,14 @@ public class CarbonNode {
     private Vector<Double> actualVector;
     private Vector<Double> previousVector;
     private double E;
-    public int neighbor1Index;
-    public int neighbor2Index;
-    public int neighbor3Index;
+
+    private Bond[] bonds;
 
     public CarbonNode(int number) {
         this.actualVector = new Vector<>(3);
         this.previousVector = new Vector<>(3);
         this.number = number;
+        this.bonds = new Bond[3];
     }
 
     public int getNumber() {
@@ -46,40 +46,11 @@ public class CarbonNode {
         E = e;
     }
 
-    public int getNeighbor1Index() {
-        return neighbor1Index;
+    public Bond[] getBonds() {
+        return bonds;
     }
 
-    public void setNeighbor1Index(int neighbor1Index) {
-        this.neighbor1Index = neighbor1Index;
-    }
-
-    public int getNeighbor2Index() {
-        return neighbor2Index;
-    }
-
-    public void setNeighbor2Index(int neighbor2Index) {
-        this.neighbor2Index = neighbor2Index;
-    }
-
-    public int getNeighbor3Index() {
-        return neighbor3Index;
-    }
-
-    public void setNeighbor3Index(int neighbor3Index) {
-        this.neighbor3Index = neighbor3Index;
-    }
-
-    @Override
-    public String toString() {
-        return "CarbonNode{" +
-                "number=" + number +
-                ", actualVector=" + actualVector +
-                ", previousVector=" + previousVector +
-                ", E=" + E +
-                ", neighbor1Index=" + neighbor1Index +
-                ", neighbor2Index=" + neighbor2Index +
-                ", neighbor3Index=" + neighbor3Index +
-                '}';
+    public void setBonds(Bond[] bonds) {
+        this.bonds = bonds;
     }
 }
