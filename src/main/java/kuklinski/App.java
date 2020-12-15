0620975f816ad6cf5e312e563160067eaf292e36
Riczard <riczardkiller@gmail.com>
2020-12-15 20:45:16 +0100

    Update App.java

--- src/main/java/kuklinski/App.java
+++ src/main/java/kuklinski/App.java
@@ -1,7 +1,6 @@
 package kuklinski;
 
 import kuklinski.calculation.Case1;
-
 import kuklinski.calculation.Case2;
 import kuklinski.calculation.Case3;
 import kuklinski.model.CarbonNode;
@@ -10,6 +9,10 @@ import kuklinski.model.Fullerene;
 import java.util.Arrays;
 import java.util.List;
 
+
+enum Persons {PK,JK};
+//enum Types {type1,type2,type3};
+
 //  C   14    0.684800    0.000000   -3.449997    1   18   39   41
 //  C   24    0.684800    0.000000    3.449997    1   23   25   51
 //  C   41   -0.684800    0.000000   -3.449997    1   14   42   45
@@ -17,21 +20,28 @@ import java.util.List;
 
 
 public class App {
-
+    public enum Types {type1,type2,type3};
+    public static Types type;
     public static void main(String[] args) {
+
         int numberOfAtoms = 0;
-        List<String> data = Parsers.getFullereneDataFromTXT("c60card.txt");
+        Persons person = Persons.JK;
+        List<String> data = Parsers.getFullereneDataFromTXT("c60card.txt", person);
 
         Fullerene fullerene = new Fullerene(Integer.parseInt(data.get(numberOfAtoms)));
         data.remove(numberOfAtoms);
         fullerene.fill(data);
         fullerene.calculateR0();
         fullerene.calculateR();
-        //prosze wybrac tylko 1 case, reszte zakomentowac
+        fullerene.setStartVector();
 
-//        Case1.calculateCase1(fullerene);
-//        Case2.calculateCase2(fullerene);
-        Case3.calculateCase3(fullerene);
+        type = Types.type3;
+
+        switch (type) {
+            case type1: Case1.calculateCase1(fullerene); break;
+            case type2: Case2.calculateCase2(fullerene); break;
+            case type3: Case3.calculateCase3(fullerene); break;
+        }
     }
 
 }
